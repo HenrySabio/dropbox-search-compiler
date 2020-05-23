181b9a44d4294e2a57d1b8295d42ecb04ad7df40
Henry Sabio <hsabio.a@gmail.com>
2020-05-23 19:25:23 -0400

    Setup & tested api connection

--- dropbox-query-get.js
+++ dropbox-query-get.js
@@ -1,3 +1,24 @@
+require('dotenv').config();
+require('isomorphic-fetch'); // or another library of choice.
+const Dropbox = require('dropbox').Dropbox;
+const dbx = new Dropbox({ accessToken: process.env.API_KEY });
+
+dbx.usersGetCurrentAccount()
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+
+dbx.filesListFolder({ path: '' })
+    .then(function (response) {
+        console.log(response.entries);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+
 const fs = require('fs');
-const queryArray = fs.readFileSync('query.txt').toString().split('\n');
-console.log(queryArray);
+const productArray = fs.readFileSync('query.txt').toString().split('\n');
+console.log(productArray);
