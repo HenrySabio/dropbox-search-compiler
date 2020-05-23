2bb6c553fddb573aa634a640e40f1df7824999f4
Henry Sabio <hsabio.a@gmail.com>
2020-05-23 19:45:05 -0400

    Created picSearch function - returns found results

--- dropbox-query-get.js
+++ dropbox-query-get.js
@@ -3,22 +3,18 @@ require('isomorphic-fetch'); // or another library of choice.
 const Dropbox = require('dropbox').Dropbox;
 const dbx = new Dropbox({ accessToken: process.env.API_KEY });
 
-dbx.usersGetCurrentAccount()
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
-
-dbx.filesListFolder({ path: '' })
-    .then(function (response) {
-        console.log(response.entries);
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
-
 const fs = require('fs');
 const productArray = fs.readFileSync('query.txt').toString().split('\n');
 console.log(productArray);
+
+function picSearch(searchQuery) {
+    dbx.filesSearch({ path: '', mode: 'filename_and_content', max_results: 1, query: searchQuery })
+        .then(function (response) {
+            console.log(response.matches);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+}
+
+picSearch('purple');
