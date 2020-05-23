527875e17f9de5ebbd77a41c5f7ab1ed07ed8817
Henry Sabio <hsabio.a@gmail.com>
2020-05-22 21:33:31 -0400

    Refactor read file function

--- dropbox-query-get.js
+++ dropbox-query-get.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
-fs.readFile('query.txt', 'utf8', function(err, data) {
-    if (err) {
-        return console.log(err); 
-    } else {
-        let queryArray = data.split('\n');
-        console.log(queryArray);
-    }
+fs.readFile('query.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    let queryArray = data.split('\n');
+    console.log(queryArray);
 });
