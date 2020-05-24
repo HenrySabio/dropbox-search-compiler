9e7ad191fc1cfed710376646a6c55e9fe1770fc3
Henry Sabio <hsabio.a@gmail.com>
2020-05-23 20:27:19 -0400

    Created fileCopy function - copy found files to new folder for sharing

--- dropbox-query-get.js
+++ dropbox-query-get.js
@@ -7,14 +7,27 @@ const fs = require('fs');
 const productArray = fs.readFileSync('query.txt').toString().split('\n');
 console.log(productArray);
 
-function picSearch(searchQuery) {
+
+function dropboxSearch(searchQuery) {
     dbx.filesSearch({ path: '', mode: 'filename_and_content', max_results: 1, query: searchQuery })
-        .then(function (response) {
-            console.log(response.matches);
+        .then(function (res) {
+            let originalPath = res.matches[0].metadata.path_lower;
+            let fileName = res.matches[0].metadata.name;
+            copyFile(originalPath, fileName);
         })
         .catch(function (error) {
             console.error(error);
         });
 }
 
-picSearch('purple');
+function copyFile(originalPath, fileName) {
+    dbx.filesCopy({ allow_shared_folder: true, autorename: true, from_path: originalPath, to_path: `/requested-files/${fileName}` })
+                .then(function (res) {
+                    console.log(res);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+}
+
+dropboxSearch('photo');
